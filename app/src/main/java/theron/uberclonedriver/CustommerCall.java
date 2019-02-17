8799a958e27bdd7521f45518006104e15b52a929
EduAkMi <edu_misina@hotmail.com>
2019-02-17 19:10:48 -0300

    10th video - Decline Request button working

--- app/src/main/java/theron/uberclonedriver/CustommerCall.java
+++ app/src/main/java/theron/uberclonedriver/CustommerCall.java
@@ -6,8 +6,11 @@ import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.LinearInterpolator;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,16 +34,24 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import theron.uberclonedriver.Common.Common;
+import theron.uberclonedriver.Model.FCMResponse;
+import theron.uberclonedriver.Model.Notification;
+import theron.uberclonedriver.Model.Sender;
+import theron.uberclonedriver.Model.Token;
 import theron.uberclonedriver.Remote.IFCMService;
 import theron.uberclonedriver.Remote.IGoogleAPI;
 
 public class CustommerCall extends AppCompatActivity {
 
     TextView txtTime, txtAddress, txtDistance;
+    Button btnCancel, btnAccept;
 
     MediaPlayer mediaPlayer;
 
     IGoogleAPI mService;
+    IFCMService mFCMService;
+
+    String customerId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +59,23 @@ public class CustommerCall extends AppCompatActivity {
         setContentView(R.layout.activity_custommer_call);
 
         mService = Common.getGoogleAPI();
+        mFCMService = Common.getFCMService();
 
         //InitView
         txtTime = findViewById(R.id.txtTime);
         txtAddress = findViewById(R.id.txtAddress);
         txtDistance=findViewById(R.id.txtDistance);
+
+        btnAccept = findViewById(R.id.btnAccept);
+        btnCancel = findViewById(R.id.btnDecline);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!TextUtils.isEmpty(customerId))
+                    cancelBooking(customerId);
+            }
+        });
 
         mediaPlayer = MediaPlayer.create(this, R.raw.ringtone);
         mediaPlayer.setLooping(true);
@@ -61,9 +84,33 @@ public class CustommerCall extends AppCompatActivity {
         if(getIntent() != null){
             double lat = getIntent().getDoubleExtra("lat", -1.0);
             double lng = getIntent().getDoubleExtra("lng", -1.0);
+            customerId = getIntent().getStringExtra("customer");
 
             getDirection(lat, lng);
         }
+    }
+
+    private void cancelBooking(String customerId) {
+        Token token = new Token(customerId);
+
+        Notification notification = new Notification("Notice!", "Driver has cancelled your request");
+        Sender sender = new Sender(token.getToken(), notification);
+
+        mFCMService.sendMessage(sender)
+                .enqueue(new Callback<FCMResponse>() {
+                    @Override
+                    public void onResponse(Call<FCMResponse> call, Response<FCMResponse> response) {
+                        if(response.body().success == 1){
+                            Toast.makeText(CustommerCall.this, "Cancelled!", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<FCMResponse> call, Throwable t) {
+
+                    }
+                });
     }
 
     private void getDirection(double lat, double lng) {
